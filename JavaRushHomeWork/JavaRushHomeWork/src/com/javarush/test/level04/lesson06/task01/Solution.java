57fcef8b89e2812085fca9a121f5750c853f97d7
motokotik <bidnik.alina@outlook.com>
2018-11-08 15:46:35 +0200

    add com.javarush.test.level04.lesson06.task01

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level04/lesson06/task01/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level04.lesson06.task01;
+
+/* Minimum of two numbers
+Write a program that reads two numbers from keyboard and displays to the screen the minimum of these numbers.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        if (a < b) System.out.println(a);
+        else System.out.println(b);
+    }
+}
