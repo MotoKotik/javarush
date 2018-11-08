f4cb24f0562f70e612c84bb404eab649b3dfb3f1
motokotik <bidnik.alina@outlook.com>
2018-11-08 15:27:05 +0200

    add com.javarush.test.level03.lesson08.task01

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level03/lesson08/task01/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level03.lesson08.task01;
+
+/* How to conquer the world
+Write a program that reads from keyboard a name and a number and displays the text:
+    «name» will conquer the world in «number» years. Bwahaha!
+    (The sequence of input data is important.)
+
+    Example:
+    Joe will conquer the world in 8 years. Bwahaha!
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(reader.readLine());
+        String name = reader.readLine();
+
+        System.out.println(name + " will conquer the world in " + number + " years. Bwahaha!");
+    }
+}
