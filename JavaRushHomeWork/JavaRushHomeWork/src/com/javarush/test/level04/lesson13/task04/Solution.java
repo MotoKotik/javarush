b5a20666d089fb1c86acff918a592795bc98a02e
motokotik <bidnik.alina@outlook.com>
2018-11-11 14:08:03 +0200

    add com.javarush.test.level04.lesson13.task04

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level04/lesson13/task04/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level04.lesson13.task04;
+
+import java.io.*;
+
+/* Draw lines
+Write a program that displays
+    -	a horizontal line of 10 eights
+    -	a vertical line of 10 eights
+    Use “for” loop.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        for (int i = 0; i < 10; i++) System.out.print("8");
+        System.out.println();
+        for (int i = 0; i < 10; i++) System.out.println("8");
+    }
+}
