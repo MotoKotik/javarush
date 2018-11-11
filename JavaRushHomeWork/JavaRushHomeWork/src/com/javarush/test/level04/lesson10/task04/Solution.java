bafbe87ea8920bcbf4fe0aef7ca18baba6f82f37
motokotik <bidnik.alina@outlook.com>
2018-11-11 13:58:39 +0200

    add com.javarush.test.level04.lesson10.task04

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level04/lesson10/task04/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level04.lesson10.task04;
+
+import java.io.*;
+
+/* S-square
+Write a program that displays to the screen a square 10x10 of “S” characters. Use “while” loop.
+    Don’t separate characters in the same line.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        int i = 0;
+        while (i++ < 10){
+            int j = 0;
+            while (j++ < 10) System.out.print("S");
+            System.out.println();
+        }
+
+    }
+}
