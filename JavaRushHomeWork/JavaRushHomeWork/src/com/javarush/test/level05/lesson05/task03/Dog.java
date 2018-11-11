842ae58279e4df0b90211dddba1e7080a82462d0
motokotik <bidnik.alina@outlook.com>
2018-11-11 14:40:41 +0200

    add com.javarush.test.level05.lesson05.task03

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level05/lesson05/task03/Dog.java
@@ -0,0 +1,31 @@
+package com.javarush.test.level05.lesson05.task03;
+
+/* Getters and setters for the class Dog
+Create a class Dog. A dog must have a name - String name and age - int age.
+    Create getters and setters for all the variables of the Dog class.
+*/
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
