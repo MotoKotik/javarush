9a5d0b182eecf3bca8475165413d0aaa030e0435
motokotik <bidnik.alina@outlook.com>
2018-11-11 15:33:04 +0200

    add com.javarush.test.level05.lesson09.task05

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level05/lesson09/task05/Rectangle.java
@@ -0,0 +1,54 @@
+package com.javarush.test.level05.lesson09.task05;
+
+/* Create a class Rectangle
+Create a class Rectangle. Its data will be top, left, width and height. Create for it as much constructors as possible:
+    Examples:
+    -	4 parameters are set: left, top, width, height
+    -	width/height are not set (both equal 0)
+    -	height are not set (equal to width), create a square
+    -	create a copy of another rectangle (it’s passed in the parameters)
+*/
+
+public class Rectangle
+{
+    private int top;
+    private int left;
+    private int width;
+    private int height;
+
+    public Rectangle(int top, int left, int width, int height){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(int top, int left){
+        this.top = top;
+        this.left = left;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public Rectangle(int top, int left, int width){
+        this.top = top;
+        this.left = left;
+        this.width = width;
+        this.height = width;
+    }
+
+    public Rectangle(Rectangle rectangle){
+        this.top = rectangle.top;
+        this.left = rectangle.left;
+        this.width = rectangle.width;
+        this.height = rectangle.height;
+    }
+
+    public Rectangle(int top){
+        this.top = top;
+        this.left = 0;
+        this.width = 0;
+        this.height = 0;
+    }
+
+}
