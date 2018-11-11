cc76ac20eb667c344e3880a5da5bb4c53590242b
motokotik <bidnik.alina@outlook.com>
2018-11-11 16:01:25 +0200

    add com.javarush.test.level05.lesson12.home04

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level05/lesson12/home04/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level05.lesson12.home04;
+
+/* Display the current date
+Display to the screen the current date in a form similar to «21 02 2014».
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println("11 11 2018");
+    }
+}
