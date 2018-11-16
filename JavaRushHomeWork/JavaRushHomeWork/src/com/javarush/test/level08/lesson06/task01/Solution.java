00923f3604df94755ac8acc80632357dab1b5620
motokotik <bidnik.alina@outlook.com>
2018-11-16 14:36:20 +0200

    add com.javarush.test.level08.lesson06.task01

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level08/lesson06/task01/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level08.lesson06.task01;
+
+import java.util.*;
+
+/* Create two lists LinkedList & ArrayList.
+Create two lists: LinkedList and ArrayList.
+*/
+
+public class Solution
+{
+    public static Object createArrayList()
+    {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        return arrayList;
+
+    }
+
+    public static Object createLinkedList()
+    {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        return linkedList;
+
+    }
+}
