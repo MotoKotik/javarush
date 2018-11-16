7a5fa861817f3ba1b0f6776349497083640f6e43
motokotik <bidnik.alina@outlook.com>
2018-11-16 14:42:39 +0200

    add com.javarush.test.level08.lesson06.task05

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level08/lesson06/task05/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level08.lesson06.task05;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/* Four methods
+Implement 4 methods. The methods should return a list best suited to perform specified operations (to cope fast with a large number of operations). No measurement needed.
+*/
+
+public class Solution
+{
+    public static List  getListForGet()
+    {
+        return new ArrayList();
+
+    }
+
+    public static List  getListForSet()
+    {
+        return new ArrayList();
+
+    }
+
+    public static List  getListForAddOrInsert()
+    {
+        return new LinkedList();
+
+    }
+
+    public static List  getListForRemove()
+    {
+        return new LinkedList();
+
+    }
+}
