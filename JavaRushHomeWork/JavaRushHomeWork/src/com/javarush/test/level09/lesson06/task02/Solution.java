56dc4620afd1157aaf0155de2c0bf10b60a950a2
motokotik <bidnik.alina@outlook.com>
2018-11-18 19:11:45 +0200

    add com.javarush.test.level09.lesson06.task02

---
+++ JavaRushHomeWork/JavaRushHomeWork/src/com/javarush/test/level09/lesson06/task02/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level09.lesson06.task02;
+
+/* Exception when operating with strings
+Catch the exception that occurs when run the code:
+    String s = null;
+    String m = s.toLowerCase();
+    Display the exception to the screen, specifying its type.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        try
+        {
+            String s = null;
+            String m = s.toLowerCase();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+}
